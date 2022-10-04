3518d8ff134aba17125bf75eae793417c08ad662
Esteban Piazza <estebanpiazza51@gmail.com>
2022-10-04 20:14:26 -0300

    sumada ejercitcion

--- clase 10/script.js
+++ clase 10/script.js
@@ -55,3 +55,10 @@ let edadARestar = prompt("Ingrese su edad:")
 console.log(typeof(edadARestar))
 alert ("Su edad el año pasado era de: " + (parseInt(edadARestar) + 1))
 
+/*
+Ejercicios para practicar:
+1) Pedir un numero y mostrar su doble, su mitad, el siguiente y el anterior
+2) Pedir nombre y apellido y mostrar la siguiente frase: "Su nombre es (Nombre) y su apellido es (apellido)"
+3) Pedir al usuario 2 numeros y mostrar true si el primero es mayor al segundo y false si el segundo es mayor
+4) Pedir al usuario un numero y mostrar 0 si es para y 1 si es impar.
+*/
